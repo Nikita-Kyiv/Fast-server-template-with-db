b37e450f5abe5704882b65e4830f8195d69ca316
Nikita <10whats10@gmail.com>
2026-06-11 01:08:06 +0300

    fast template change

--- app/db/CRUD/Example.py
+++ app/db/CRUD/Example.py
@@ -5,14 +5,14 @@ import app.db.schemas
 
 class ExampleCRUD:
     @staticmethod
-    async def create_example(session: AsyncSession, example_data: dict) -> app.db.schemas.models:
-        stmt = insert(app.db.schemas.models).values(**example_data).returning(app.db.schemas.models)
+    async def create_example(session: AsyncSession, example_data: dict) -> app.db.schemas.Example:
+        stmt = insert(app.db.schemas.Example).values(**example_data).returning(app.db.schemas.Example)
         result = await session.execute(stmt)
         await session.commit()
         return result.scalar_one()
 
     @staticmethod
     async def get_example_by_id(session: AsyncSession, example_id: int) -> app.db.schemas.models:
-        stmt = select(app.db.schemas.models).where(app.db.schemas.models.id == example_id)
+        stmt = select(app.db.schemas.Example).where(app.db.schemas.models.id == example_id)
         result = await session.execute(stmt)
         return result.scalar_one_or_none()
